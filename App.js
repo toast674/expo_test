99385afd208bec51ed3c4f3b809230301ea63a16
hiromasa <toast674@gmail.com>
2020-01-19 11:28:11 +0900

    install navigation module

--- App.js
+++ App.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import Appbar from './src/components/Appbar';
-import SignupScreen from './src/screens/SignupScreen';
+import MemoListScreen from './src/screens/MemoListScreen';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Appbar />
-        <SignupScreen />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFDF6',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 78,
-  },
+const App = createStackNavigator({
+  Home: { screen: MemoListScreen },
 });
+
+export default createAppContainer(App);
