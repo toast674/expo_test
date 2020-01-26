0a653d93604133b84397a5c0f45375acd3c62330
hiromasa <toast674@gmail.com>
2020-01-26 16:48:57 +0900

    hide firebase certificate

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
+import firebase from 'firebase';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
+import ENV from './env.json';
+
+const firebaseConfig = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PROJECT_ID,
+  storageBucket: ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+  appId: ENV.FIREBASE_APP_ID,
+};
+firebase.initializeApp(firebaseConfig);
+
 const App = createStackNavigator(
   {
+    Login:      { screen: LoginScreen },
     MemoDetail: { screen: MemoDetailScreen },
     MemoEdit:   { screen: MemoEditScreen },
     Home:       { screen: MemoListScreen },
-    Login:      { screen: LoginScreen },
     Signup:     { screen: SignupScreen },
   },
   {
